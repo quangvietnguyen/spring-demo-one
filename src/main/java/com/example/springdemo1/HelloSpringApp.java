3e586ccbe2404f3bd433cfd318c34e1dcbf89ad3
nguye374 <nguye374@sheridancollege.ca>
2022-07-13 01:46:12 -0400

    create spring container with xml

--- src/main/java/com/example/springdemo1/HelloSpringApp.java
+++ src/main/java/com/example/springdemo1/HelloSpringApp.java
@@ -7,9 +7,10 @@ public class HelloSpringApp {
         //load the spring config file
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //retrieve bean from spring container
-
+        Coach theCoach = context.getBean("myCoach", Coach.class);
         //call methods on the bean
-
+        System.out.println(theCoach.getDailyWorkout());
         //close the context
+        context.close();
     }
 }
